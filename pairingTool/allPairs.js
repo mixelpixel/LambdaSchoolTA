4e88e20b9f41a2267cbc72d4a65274d2a3905988
mixelpixel <mixelpix@mac.com>
2018-02-04 02:32:52 -0500

    pair programming partners

--- pairingTool/allPairs.js
+++ pairingTool/allPairs.js
@@ -6,8 +6,7 @@ let students = fs.readFileSync(filePath, 'utf8').split(os.EOL);
 students.pop(); // gets ride of trailing comma
 // const students = ['fred', 'ted', 'bob', 'alice', 'jane', 'jenny', 'frank', 'betty'];
 if (students % 2 != 0) {
-  console.log(`There's an odd number of students, we'll need to put ${students[students.length - 1]} in a triplet`);
-  // TODO: randomly select a group to assign extra students[students.length -1]
+  console.log(`Odd number of students, you need to put ${students[students.length -1]} in a triplet`)
 }
 
 const x = students.splice(0, students.length / 2) // cut the list in half
@@ -28,13 +27,19 @@ for (let i = 0; i < pairedClassSize - 1; i++) {
   studentHalf_1.rotate(1);
   for (let i = 0; i < pairedClassSize; i++) {
     pairs.push(Array(studentHalf_1[i], studentHalf_2[i]));
+    pairs.push('\n');
   }
+  pairs.push("BREAKBREAKBREAKBREAKBREAKBREAKBREAKBREAK\n\n");
 }
 // console.log(pairs);
+// fs.writeFile("test", JSON.stringify(pairs), (err) => {
 
-fs.writeFile("test", pairs.toString(), (err) => {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
-});
+for (let i = 0; i < pairs.length; i++) {
+  // fs.writeFile("test", JSON.stringify(pairs), (err) => {
+  fs.writeFile("test", pairs.toString(), (err) => {
+      if(err) {
+          return console.log(err);
+      }
+      // console.log("The file was saved!");
+  });
+}
