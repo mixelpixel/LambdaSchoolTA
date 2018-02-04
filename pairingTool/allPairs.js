047ff31ca23147953a585e630289232e211bb9f5
mixelpixel <mixelpix@mac.com>
2018-02-04 02:17:01 -0500

    pair programming selector

--- pairingTool/allPairs.js
+++ pairingTool/allPairs.js
@@ -1,8 +1,21 @@
-const student1 = ['fred', 'ted', 'bob', 'alice', 'jane', 'jenny', 'frank', 'betty'];
-const classSize = student1.length;
-// console.log(classSize);
-const student2 = student1.slice(); // <--- just copies all elements from source array, all new references!
-console.log(student2);
+const fs = require('fs');
+const os = require('os');
+const filePath = '../csv/students'; // or wherever your list is - note, not actuall a csv file, just a text list of names and line breajs
+
+let students = fs.readFileSync(filePath, 'utf8').split(os.EOL);
+students.pop(); // gets ride of trailing comma
+// const students = ['fred', 'ted', 'bob', 'alice', 'jane', 'jenny', 'frank', 'betty'];
+if (students % 2 != 0) {
+  console.log(`There's an odd number of students, we'll need to put ${students[students.length - 1]} in a triplet`);
+  // TODO: randomly select a group to assign extra students[students.length -1]
+}
+
+const x = students.splice(0, students.length / 2) // cut the list in half
+// make new lists
+const studentHalf_1 = x.slice()
+const studentHalf_2 = students.slice()
+
+const pairedClassSize = studentHalf_1.length;
 const pairs = [];
 
 // Rotating the array - WARNING: Mutates the array
@@ -11,67 +24,17 @@ Array.prototype.rotate = function( n ) {
   return this;
 }
 
-for (let i = 0; i < classSize - 1; i++) {
-  console.log(student1.rotate(1));
-  for (let i = 0; i < classSize; i++) {
-    pairs.push(Array(student1[i], student2[i]));
-    // console.log("iterate", pairs);
+for (let i = 0; i < pairedClassSize - 1; i++) {
+  studentHalf_1.rotate(1);
+  for (let i = 0; i < pairedClassSize; i++) {
+    pairs.push(Array(studentHalf_1[i], studentHalf_2[i]));
   }
-  pairs.push("BREAKBREAKBREAK");
 }
-console.log(pairs);
-
-
-
-
-// const fs = require('fs');
-// const os = require('os');
-// const filePath = '../csv/students.csv';
-//
-// let csv = fs.readFileSync(filePath, 'utf8').split(',');
-// // csv.pop();
-// // csv.pop();
-//
-//
-// // console.log('Initial File content: ' + csv);
-// // console.log(typeof(csv));
-// // console.log(typeof(csv[0]));
-// // console.log(csv);
-// // const student1 = csv.split(',');
-// // console.log(student1);
-//
-//
-// const student1 = ['fred', 'ted', 'bob', 'alice', 'jane', 'jenny', 'frank', 'betty'];
-// // const student1 = csv.slice(0, csv.length - 2); // chops off command and consultant
-// const classSize = student1.length;
-// // console.log(classSize);
-// const student2 = student1.slice(); // <--- just copies all elements from source array, all new references!
-// // console.log(student2.length);
-// const pairs = [];
-//
-// // Rotating the array - WARNING: Mutates the array
-// Array.prototype.rotate = function( n ) {
-//   this.unshift.apply( this, this.splice( n, this.length ) )
-//   return this;
-// }
-//
-// student1.forEach((x) => {
-//   pairs.push(new Array);
-// })
-//
 // console.log(pairs);
-//
-// for (let i = 0; i < classSize - 1; i++) {
-// // for (let i = 0; i < 4 - 1; i++) {
-//   console.log(student1.rotate(1));
-//
-//   for (let j = 0; j < classSize; j++) {
-//   // for (let j = 0; j < 5; j++) {
-//     pairs[j].push(Array(student1[j], student2[j]));
-//     // console.log(pairs[j]);
-//     // console.log("iterate", pairs);
-//   }
-//   pairs.push("BREAKBREAKBREAK");
-// }
-// // console.log(pairs.length);
-// console.log(pairs);
+
+fs.writeFile("test", pairs.toString(), (err) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("The file was saved!");
+});
