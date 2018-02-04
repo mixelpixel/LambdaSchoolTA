26283d1b53bdeaaf185f251069e5416a3b19f7aa
mixelpixel <mixelpix@mac.com>
2018-02-04 10:47:10 -0500

    pair programming partners

--- pairingTool/allPairs.js
+++ pairingTool/allPairs.js
@@ -1,31 +1,55 @@
+/*
+ * I just copied the list of names from the cohorts student list on Repl.it
+ * Ideally there's a better way (Airtable?) to access said list, but...
+ * This program is expecting a text file with just a lit of names, e.g.
+ Ronelle Lawson
+ Igor Yermak
+ Ashlei Jones
+ Steven Magadan
+ Glenn-David Daniel
+ Dixie Korley
+ * names, newlines. That's all. It will cut out the empty string if there's an newline at the text file end.
+ * there's a lot of room for improvement
+ * the resulting text file still needs some massaging.
+ */
+
 const fs = require('fs');
 const os = require('os');
-const filePath = './students'; // or wherever your list is - note, not actuall a csv file, just a text list of names and line breajs
+const filePath = './students'; // or wherever your list is - note: just a text list of names and line breaks
 
-let students = fs.readFileSync(filePath, 'utf8').split(os.EOL);
-students.pop(); // gets ride of trailing comma
 // const students = ['fred', 'ted', 'bob', 'alice', 'jane', 'jenny', 'frank', 'betty'];
-if (students % 2 != 0) {
-  console.log(`Odd number of students, you need to put ${students[students.length -1]} in a triplet`)
+// const students = 'fred\nted\nbob\nalice\njane\njenny\nfrank\nbetty';
+const students = fs.readFileSync(filePath, 'utf8').split(os.EOL);
+
+// Get rid of empty string at end of array after split
+if (students[students.length - 1] === '') {
+  students.pop();
 }
 
-const x = students.splice(0, students.length / 2) // cut the list in half
+// Odd number of students?
+if (students % 2 != 0) {
+  console.log(`That's ODD - there are ${students.length} students.\nYou need to put ${students[students.length -1]} in a triplet`);
+  // TODO: per pair iteration, randomly select a student from odd lists and assign to triplet
+}
+
+// cut the list in half - MUTATES original list
+const x = students.splice(0, students.length / 2)
 // make new lists
 const studentHalf_1 = x.slice()
 const studentHalf_2 = students.slice()
 
-const pairedClassSize = studentHalf_1.length;
+const numOfPairs = studentHalf_1.length;
 const pairs = [];
 
 // Rotating the array - WARNING: Mutates the array
-Array.prototype.rotate = function( n ) {
-  this.unshift.apply( this, this.splice( n, this.length ) )
+Array.prototype.rotate = function(n) {
+  this.unshift.apply(this, this.splice(n, this.length ))
   return this;
 }
 
-for (let i = 0; i < pairedClassSize - 1; i++) {
+for (let i = 0; i < numOfPairs - 1; i++) {
   studentHalf_1.rotate(1);
-  for (let i = 0; i < pairedClassSize; i++) {
+  for (let i = 0; i < numOfPairs; i++) {
     pairs.push(Array(studentHalf_1[i], studentHalf_2[i]));
     pairs.push('\n');
   }
